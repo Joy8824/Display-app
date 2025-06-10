4598ead9ac2c07d6c596737eeb2f55390400f779
Joy8824 <joydisplays8@outlook.com>
2025-06-10 14:10:48 -0500

    Update review.js

--- pages/review.js
+++ pages/review.js
@@ -1,7 +1,6 @@
 // pages/review.js
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
-import '/src/output.css';
 
 export default function ReviewPage() {
   const router = useRouter();
