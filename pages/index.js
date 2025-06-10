802c726b880129a8f14bd67ffba7ba88356a11cb
Joy8824 <joydisplays8@outlook.com>
2025-06-10 13:34:25 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,9 @@
 export default function Home() {
   return (
-    <div className="h-screen flex items-center justify-center text-2xl font-semibold">
-      Hello from Display App
+    <div className="bg-blue-500 text-white p-6 rounded-xl">
+    If you see a blue box, Tailwind is working!
     </div>
-  <div className="bg-blue-500 text-white p-6 rounded-xl">
-  If you see a blue box, Tailwind is working!
-</div>
+  
   
   );
 }
