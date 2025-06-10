f2b66c565803a317068f093cdc29a5a51330aad8
Joy8824 <joydisplays8@outlook.com>
2025-06-10 13:40:14 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
-import '../styles/globals.css'; //  This line is critical
+import '../styles/globals.css';
+
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
