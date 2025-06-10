8cc3cace133bc05831816c13b8d4cb3c25c343d4
Joy8824 <joydisplays8@outlook.com>
2025-06-10 14:27:52 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="bg-blue-500 text-white p-6 rounded-xl">
+    <div className="bg-avocado-100 p-6 rounded-xl">
     If you see a blue box, Tailwind is working!
     </div>
   
