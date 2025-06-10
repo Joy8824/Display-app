91e231bafef07b74ab42f221c419b491bc86caa3
Joy8824 <joydisplays8@outlook.com>
2025-06-10 10:32:15 -0500

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="h-screen flex items-center justify-center text-2xl font-semibold">
+      Hello from Display App
+    </div>
+  );
+}
