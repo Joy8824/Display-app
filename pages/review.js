ea88ebe05466e8d8bd31da6098c1a2c72f362228
Joy8824 <joydisplays8@outlook.com>
2025-06-30 09:14:47 -0500

    changed lg to md

--- pages/review.js
+++ pages/review.js
@@ -39,7 +39,7 @@ return (
   <div className="flex flex-col items-center w-full p-6 bg-white">
     <h1 className="text-3xl font-semibold text-center mb-6">Graphic Proofs</h1>
 
-    <div className="flex-auto flex-col lg:flex-row w-full max-w-6xl gap-6 h-[100vh]">
+    <div className="flex-auto flex-col md:flex-row w-full max-w-6xl gap-6 h-[100vh]">
       
       {/* Left side: PDF + Buttons (2/3 width) */}
       <div className="w-2/3 flex flex-col border border-gray-300 rounded bg-white shadow overflow-hidden">
