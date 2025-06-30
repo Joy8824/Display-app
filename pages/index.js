c78d16db49a9171c9f9d1748e645c52c3702d841
Joy8824 <joydisplays8@outlook.com>
2025-06-30 11:04:36 -0500

    checking to see if bg gray works

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 export default function Home() {
   return (
-<div className="bg-blue-500 text-white p-6 rounded-xl">
-  If you see a blue box, Tailwind is working!
-</div>
+
 <div className="bg-gray-800 text-white p-4">Should be dark gray</div>
+
+
+
+
 
   
   
