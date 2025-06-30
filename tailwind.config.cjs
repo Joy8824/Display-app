c201b9d0505f2df1d12d5279e276614c668c643c
Joy8824 <joydisplays8@outlook.com>
2025-06-30 10:59:19 -0500

    Update tailwind.config.cjs
    updated to catch anything else including bg colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,10 @@
-// tailwind.config.cjs
-module.exports = {
+export default {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
     './src/**/*.{js,ts,jsx,tsx}',
+    './layouts/**/*.{js,ts,jsx,tsx}',
+    './**/*.{js,ts,jsx,tsx}', // <-- catches anything else
   ],
   theme: {
     extend: {},
