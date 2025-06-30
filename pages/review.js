5ff742dadbc4ba0fdec0ee9200286360321c419a
Joy8824 <joydisplays8@outlook.com>
2025-06-30 09:48:45 -0500

    ...

--- pages/review.js
+++ pages/review.js
@@ -69,8 +69,9 @@ return (
         </div>
       </div>
 
-      {/* Right side: Comments (1/3 width) 
+     
     </div>
+     {/* Right side: Comments (1/3 width) 
     <div className="w-1/3 flex flex-col border border-gray-300 rounded p-4 bg-white shadow">
         <label htmlFor="comments" className="font-semibold mb-2">
           Comments
